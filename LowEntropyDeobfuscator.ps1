a1d61815ca90b66d55cfe9bf95518fa86bfbdabc
Jacopo Talamini <talamini.jacopo@gmail.com>
2023-11-14 11:44:09 +0100

    Update LowEntropyDeobfuscator.ps1

--- LowEntropyDeobfuscator.ps1
+++ LowEntropyDeobfuscator.ps1
@@ -1,12 +1,20 @@
+<#
+.SYNOPSIS
+    PowerShell script used to implement low-entropy strings de-obfuscation.
+.DESCRIPTION
+    This script allows to de-obfuscate a string based on a given wordlist. The script must be executed on the same wordlist used to obfuscate the string.
+    This obfuscation method can be useful when dealing with entropy-based malware detection tools.
+#>
+
 param (
-    [string]$wordFile
+    [string]$wordListPath
 )
-if (-not $wordFile) {
-    Write-Host "Usage: LowEntropyDeobfuscator.ps1 -wordFile <path>"
+if (-not $wordListPath) {
+    Write-Host "Usage: LowEntropyDeobfuscator.ps1 -wordListPath <path>"
     exit
 }
 # read unique words from file
-$uniqueWords = $(Get-Content $wordFile | Sort-Object | Get-Unique | Select-Object -First 256)
+$uniqueWords = $(Get-Content $wordListPath | Sort-Object | Get-Unique | Select-Object -First 256)
 # create an empty list
 $wordList = @()
 # add each word to the list
@@ -14,7 +22,7 @@ foreach ($word in $uniqueWords) {
     $wordList += $word
 }
 # obfuscated list
-# YOUR OBFUSCATED PAYLOAD HERE
+# YOUR OBFUSCATED PAYLOAD GOES HERE
 $obfuscated =@("avvicinare","battaglia","bestia","bambino","bestia","azione","avvicinare","battere","avvocato","biondo","badare","badare","baciare","bere")
 # convert each byte into a word
 $output = ""
